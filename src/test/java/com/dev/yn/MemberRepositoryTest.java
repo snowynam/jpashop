be465d689760c6d7eec4938f9e6163bbbf510f07
snowynam <snowynam1@gmail.com>
2020-08-19 18:55:36 +0900

    junit Test 4 -> 5 로변경

--- src/test/java/com/dev/yn/MemberRepositoryTest.java
+++ src/test/java/com/dev/yn/MemberRepositoryTest.java
@@ -1,18 +1,18 @@
 package com.dev.yn;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.yn.Model.Member;
 import com.dev.yn.repository.MemberRepository;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
