03c20a643eb20d2f3a8d077fee81dcb4610b9e88
snowynam <snowynam@ssenstone.com>
2020-09-09 09:35:30 +0900

    MemberRepository

--- src/main/java/com/dev/yn/repository/MemberRepository.java
+++ src/main/java/com/dev/yn/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.dev.yn.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -20,5 +22,15 @@ public class MemberRepository {
 	
 	public Member find(Long id) {
 		return em.find(Member.class, id);
+	}
+	
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class).getResultList();
+	}
+	
+	public List<Member> findByName(String name){
+		return em.createQuery("select m from Member m where m.name = :name", Member.class)
+				.setParameter("name", name)
+				.getResultList();
 	}
 }
