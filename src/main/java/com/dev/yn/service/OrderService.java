56aba45b2e5ec853ec97409d3790f1087d08af69
snowynam <snowynam@ssenstone.com>
2020-09-10 11:05:22 +0900

    Order서비스 개발 - 주문

---
+++ src/main/java/com/dev/yn/service/OrderService.java
@@ -0,0 +1,55 @@
+package com.dev.yn.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.yn.domain.Delivery;
+import com.dev.yn.domain.Member;
+import com.dev.yn.domain.Order;
+import com.dev.yn.domain.OrderItem;
+import com.dev.yn.domain.item.Item;
+import com.dev.yn.repository.ItemRepository;
+import com.dev.yn.repository.MemberRepository;
+import com.dev.yn.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+	
+	private final OrderRepository orderRepository;
+	private final MemberRepository memberRepository;
+	private final ItemRepository itemRepository;
+	
+	/**
+	 *주문 
+	 */
+	@Transactional
+	public Long order(Long memberId, Long itemId, int count) {
+		
+		//엔티티 조회
+		Member member = memberRepository.findOne(memberId);
+		Item item = itemRepository.findOne(itemId);
+		
+		//배송정보 생성
+		Delivery delivery = new Delivery();
+		delivery.setAddress(member.getAdress());
+		
+		//주문상품 생성
+		OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+		
+		//주문 생성
+		Order order = Order.createOrder(member, delivery, orderItem);
+		
+		//주문 저장
+		orderRepository.save(order);
+		
+		return order.getId();
+	}
+	
+	//취소
+	
+	//검색
+}
