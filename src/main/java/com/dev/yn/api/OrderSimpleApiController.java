45c55b3fce78e13071ea71190576ccc02fbc6d9f
snowynam <snowynam@ssenstone.com>
2020-09-16 15:04:16 +0900

    order 조회 version2

--- src/main/java/com/dev/yn/api/OrderSimpleApiController.java
+++ src/main/java/com/dev/yn/api/OrderSimpleApiController.java
@@ -1,14 +1,19 @@
 package com.dev.yn.api;
 
+import com.dev.yn.domain.Address;
 import com.dev.yn.domain.Order;
+import com.dev.yn.domain.OrderStatus;
 import com.dev.yn.repository.OrderRepository;
 import com.dev.yn.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne
@@ -30,5 +35,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        //ORDER 2개
+        //N + 1 -> 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+//        return orderRepository.findAllByString(new OrderSearch()).stream()
+//                .map(SimpleOrderDto::new)
+//                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();            //LAZY 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();    //LAZY 초기화
+        }
     }
 }
